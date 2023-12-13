1da692c7256e78ef2f710e79222ca4b2020e09ca
Abhishek Malve <malveabhishek2000@gmail.com>
2023-12-13 17:11:51 +0000

    print function

--- .devcontainer/basic/test.js
+++ .devcontainer/basic/test.js
@@ -1,2 +1,9 @@
-console.log("abhishek");
-console.log("engineer by proffesion")
+//console.log("abhishek");
+//console.log("engineer by proffesion")
+
+const accountId=144553
+let accountEmail=("abhi123@gmail.com");
+var accountPassward=("12345");
+let accountCity= ("jaipur");
+let accountState= ("rajastan");
+console.log(accountId,accountEmail,accountCity,accountState);
