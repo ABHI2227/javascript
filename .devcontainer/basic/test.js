55e6aa439a336d63670c8699b98f22643f796a8f
Abhishek Malve <malveabhishek2000@gmail.com>
2023-12-13 17:14:33 +0000

    console.table method

--- .devcontainer/basic/test.js
+++ .devcontainer/basic/test.js
@@ -6,4 +6,5 @@ let accountEmail=("abhi123@gmail.com");
 var accountPassward=("12345");
 let accountCity= ("jaipur");
 let accountState= ("rajastan");
-console.log(accountId,accountEmail,accountCity,accountState);
+//console.log(accountId,accountEmail,accountCity,accountState);
+console.table([accountId,accountCity,accountEmail,accountState]);
