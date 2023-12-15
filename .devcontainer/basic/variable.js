13319ccae6311cae0f907e5dbac31ce1621d652b
Abhishek Malve <malveabhishek2000@gmail.com>
2023-12-15 08:24:59 +0000

    practice

---
+++ .devcontainer/basic/variable.js
@@ -0,0 +1,8 @@
+//let Number=(2+2);
+//console.log(Number);
+
+let numOne = 50;
+let numTwo = numOne; //numTwo=numOne=50
+numOne = 100;
+console.log(numOne); //outputs 100
+console.log(numTwo); //outputs 50
